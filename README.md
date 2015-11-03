8ea8cae1d7cd217eb42e94d58a004e9691a5badf
nicozhang <nicozhangw@gmail.com>
2015-11-03 21:26:54 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# startUp
-Personal Project For The One Who Has Dream
+# D3Status
+
+Diablo3 server status notification APP, a RESTful API demo powered by Tornado.
+
+## Requirement(Python packages)
+
+ - Tornado
+ - libxml, require libxslt1-dev(libxslt) and libxml2
+ - pyquery
+ - python-dateutil
+ - celery
+ - apns([https://github.com/simonwhitaker/PyAPNs][1])
+
+
+  [1]: https://github.com/simonwhitaker/PyAPNs
---
+++ d3status/libs/options.py
@@ -0,0 +1,52 @@
+## -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 feilong.me. All rights reserved.
+#
+# @author: Felinx Lee <felinx.lee@gmail.com>
+# Created on  Jun 30, 2012
+#
+
+import logging
+import os
+import pdb
+from tornado.options import parse_command_line, options, define
+
+
+def parse_config_file(path):
+    """Rewrite tornado default parse_config_file.
+
+    Parses and loads the Python config file at the given path.
+
+    This version allow customize new options which are not defined before
+    from a configuration file.
+    """
+    config = {}
+    #pdb.set_trace()
+    execfile(path, config, config)
+    for name in config:
+        if name in options:
+            #options[name].set(config[name])
+            #options.update({name: config[name]})
+            #options.name = config[name]
+            options.__setattr__(name, config[name])
+        else:
+            define(name, config[name])
+
+
+def parse_options():
+    _root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+    _settings = os.path.join(_root, "settings.py")
+    _settings_local = os.path.join(_root, "settings_local.py")
+    #pdb.set_trace()
+    try:
+        parse_config_file(_settings)
+        logging.info("Using settings.py as default settings.")
+    except Exception, e:
+        logging.error("No any default settings, are you sure? Exception: %s" % e)
+
+    try:
+        parse_config_file(_settings_local)
+        logging.info("Override some settings with local settings.")
+    except Exception, e:
+        logging.error("No local settings. Exception: %s" % e)
+    parse_command_line()
